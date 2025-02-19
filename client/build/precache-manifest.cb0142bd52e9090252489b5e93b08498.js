fc39a83873d875cbee81c101fab375ac6c2a12d3
kent28808 <kent28808@gmail.com>
2025-02-19 10:26:35 -0800

    run build

---
+++ client/build/precache-manifest.cb0142bd52e9090252489b5e93b08498.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62c47b0db5943ab86330",
+    "url": "/static/css/main.bb223116.chunk.css"
+  },
+  {
+    "revision": "62c47b0db5943ab86330",
+    "url": "/static/js/main.6fd19dc3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "64b6befc6741cc0edc15",
+    "url": "/static/js/2.59ad7223.chunk.js"
+  },
+  {
+    "revision": "9ddd03b57fed68e358b5acfb4d381594",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1877f771c28bd820421e058eafe62c5d.js"
+  "/precache-manifest.cb0142bd52e9090252489b5e93b08498.js"
 );
 
 workbox.clientsClaim();
