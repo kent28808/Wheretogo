2c7169e7a2defa2ad16e2535608eaf8d38c1e2db
kent28808 <kent28808@gmail.com>
2025-02-18 22:08:53 -0800

    ran build command

---
+++ client/build/precache-manifest.1877f771c28bd820421e058eafe62c5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10128fb5f74bc2f14f53",
+    "url": "/static/css/main.bb223116.chunk.css"
+  },
+  {
+    "revision": "10128fb5f74bc2f14f53",
+    "url": "/static/js/main.0824bad1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "59270b01ba1c68bd0a5c",
+    "url": "/static/js/2.5d8f6c56.chunk.js"
+  },
+  {
+    "revision": "37a14c08884c161b66b97d17e4c9c812",
+    "url": "/index.html"
+  }
+];
