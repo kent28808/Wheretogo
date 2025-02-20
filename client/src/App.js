0c0a3afbeb6e7bb75fdc8fcd9f0dff9c0309f5d9
kent28808 <kent28808@gmail.com>
2025-02-19 21:51:48 -0800

    Team test

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
 import Flash from './layout/Flash.js';
